78bbc2dadc43a3b09417dc27011a0e1366e205ba
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2024-03-19 10:30:07 -0300

    Este é o código final

--- exercicio1.js
+++ exercicio1.js
@@ -1,1 +1,14 @@
-//
+let word = "Hello Human, nice to meet you!"
+
+let minusculas = word.toLowerCase();
+
+let vogais = ["a", "e", "i", "o", "u"];
+
+let caracteres = minusculas.split('');
+let vogaisFound = caracteres.filter(letra => vogais.includes(letra));
+let numero = vogaisFound.length;
+
+
+console.log("Texto: " + minusculas);
+console.log("Número de vogais: " + numero);
+console.log("Vogais encontradas: " + vogaisFound.join(", "));
